748f885ffc1b09f7da1d6ff1f6bd89c8a45345d8
Nicole <nicole.barross@eaportal.org>
2025-04-28 21:48:14 -0300

    segundo commit do java

---
+++ main.js
@@ -0,0 +1,26 @@
+/*
+let mensagem = function() {
+    console.log("Bem-vindo(a)!")
+}
+
+let somar = function(valor1, valor2) {
+    return valor1 + valor2
+}
+
+console.log(mensagem())
+console.log(somar(10, 20))
+*/
+
+let mensagem = () => {
+    console.log("Olá, tudo bem?")
+}
+
+let somar = (valor1, valor2) => valor1 + valor2
+
+let raizQuadrada = (valor1) => Math.sqrt(valor1)
+
+console.log(mensagem())
+console.log(somar(25, 768))
+console.log(raizQuadrada(49))
+
+
